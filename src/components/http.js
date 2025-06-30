b6d157a1debddfa2c30401e706eaee62dc5d1de2
Gitali03 <aonermtk@gmail.com>
2025-06-30 13:03:29 +0300

    added fetchPlaces

--- src/components/http.js
+++ src/components/http.js
@@ -6,11 +6,18 @@ export async function fetchAvailablePlaces() {
   }
   return resData.places;
 }
-
+export async function fetchUserPlaces() {
+  const response = await fetch("http://localhost:3000/user-places");
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to fetch user places");
+  }
+  return resData.places;
+}
 export async function updateUserPlaces(places) {
   const response = await fetch("http://localhost:3000/user-places", {
     method: "PUT",
-    body: JSON.stringify({places:places}),
+    body: JSON.stringify({ places: places }),
     headers: {
       "Content-Type": "application/json",
     },
@@ -19,7 +26,7 @@ export async function updateUserPlaces(places) {
   const resData = await response.json();
 
   if (!response.ok) {
-    throw new Error ('Failed to update user data');
+    throw new Error("Failed to update user data");
   }
 
   return resData.message;
